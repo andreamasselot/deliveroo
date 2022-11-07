8d3b9a4b2bea75d38de303d328e95f746153634e
Andréa Masselot <andrea.nicolas128@gmail.com>
2022-11-07 15:48:56 +0100

    top section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import { useEffect, useState } from "react";
+import axios from 'axios';
 import Header from './components/Header';
+import Section from './components/Section';
 
 function App() {
-  return (
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("https://site--deliveroo--w4jk259snrnr.code.run/");
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();},
+    []);
+
+  return isLoading ? (
+    <p>Loading... </p> 
+    ) : (
     <div className="App">
       <Header/>
-      
+      <Section name={data.restaurant.name} description={data.restaurant.description} image={data.restaurant.picture}/>
     </div>
   );
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,5 +1,15 @@
-const Section = () => {
 
+const Section = (props) => {
+return (
+  <>
+  <div>
+    <h1>{props.name}</h1>
+    <p>{props.description}</p>
+    <img className="top-section-img" src={props.image} alt="repas" />
+  </div>
+ 
+  </>
+)
 };
 
 export default Section;

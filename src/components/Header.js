8fdbb86f290ee6195c8100e01285b0dd83875475
Andréa Masselot <andrea.nicolas128@gmail.com>
2022-11-07 15:14:13 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import logo from "../assets/img/logo-teal.svg"
+const Header = () => {
+return(
+  <>
+  <header className="header">
+    <img src={logo} alt="logo" />
+  </header>
+  </>
+)
+};
+
+export default Header;
